d98fbeea92aabefb6c1b2aed36e404bde7542340
Soumen Shit <soumenshit907@gmail.com>
2024-11-16 22:05:22 +0530

    use bag for print single line

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let arr = [2, 3, 4, 5, 6, 7, 8];
+let bag = "";
 for(let i=0; i<arr.length; i++){
     if (arr[i]%2 == 0) {
-        console.log(arr[i]);
+        bag = bag + arr[i] + " ";
     }
+    console.log(bag);
 }
