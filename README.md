4dafd049325c5e5752c357cf619bd626caeffdce
Soumen Shit <soumenshit907@gmail.com>
2024-11-17 17:46:44 +0530

    Now code solved

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 ## Description
 Using this code you can find all even numbers in a array.  
+
+
+# Project Title: Find Odd Numbers in a array
+
+## Description
+Using this code you can find all odd numbers in a array. 
+
+
+# Project Title: pattren Printing
+
+## Description
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 let arr = [2, 3, 4, 5, 6, 7, 8];
-let bag = "";
+
+//Find even number the array
+let bag1 = "";
 for(let i=0; i<arr.length; i++){
     if (arr[i]%2 == 0) {
-        bag = bag + arr[i] + " ";
+        bag1 = bag1 + arr[i] + " ";
     }
-    console.log(bag);
+    console.log(bag1);
 }
+
+//Find odd number the array
+let bag2 = "";
+for(let i=0; i<arr.length; i++){
+    if (arr[i]%2 != 0) {
+        bag2 = bag2 + arr[i] + " ";
+    }
+    console.log(bag2);
+}
+
+//Pattren printing
+/*
+                    *
+                    * *
+                    * * *
+                    * * * * 
+                    * * * * *
+*/
+
+for(let i=1; i<=5; i++){
+    let bag3 = "";
+    for(let j=1; j<=i; j++){
+        bag3 = bag3+ "*" + " ";
+    }
+    console.log(bag3);
+}
+
+
